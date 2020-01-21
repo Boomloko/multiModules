7064c060eec4d3ec1be95b41f8cb683a64aaab3d
Aurélien Perraudin <orelienperraudin@gmail.com>
2020-01-21 11:38:13 +0100

    feat: multi modules

---
+++ src/app/pages/playlist/playlist-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PlaylistComponent } from './playlist.component';
+import { PlaylistDetailsComponent } from './playlist-details/playlist-details.component';
+import { CreatePlaylistComponent } from './create-playlist/create-playlist.component';
+
+const routes: Routes = [
+  { path: '', component: PlaylistComponent },
+  { path: 'playlist-details', component: PlaylistDetailsComponent },
+  { path: 'create-playlist', component: CreatePlaylistComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PlaylistRoutingModule { }
